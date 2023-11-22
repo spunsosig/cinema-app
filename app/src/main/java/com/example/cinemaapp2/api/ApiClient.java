28a5688a42926195719b0bf531c8828b2f0d7a98
spunsosig <ikram15@live.co.uk>
2023-11-22 11:54:54 +0000

    added ApiClient class to set up retrofit

--- app/src/main/java/com/example/cinemaapp2/api/ApiClient.java
+++ app/src/main/java/com/example/cinemaapp2/api/ApiClient.java
@@ -1,5 +1,15 @@
 package com.example.cinemaapp2.api;
 
-public class ApiClient {
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
+public class ApiClient {
+    private static final String BASE_URL = "https://api.themoviedb.org/3/";
+
+    public static Retrofit getClient(){
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
 }
