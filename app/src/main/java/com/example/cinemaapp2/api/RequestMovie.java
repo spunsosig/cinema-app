8f7253d9110b3d5ee7fc156257047ad32a32fb46
spunsosig <ikram15@live.co.uk>
2023-11-24 14:27:20 +0000

    TMDB api connected and logging movies

---
+++ app/src/main/java/com/example/cinemaapp2/api/RequestMovie.java
@@ -0,0 +1,14 @@
+package com.example.cinemaapp2.api;
+
+import com.example.cinemaapp2.models.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface RequestMovie {
+
+    @GET("movie/popular")
+    Call<MovieResponse> getPopularMovies(@Query("api_key") String apiKey);
+}
