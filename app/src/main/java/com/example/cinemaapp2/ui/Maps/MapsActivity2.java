09e68a7e6013873a23409fa4f394bee71818a921
spunsosig <ikram15@live.co.uk>
2024-01-03 14:36:14 +0000

    Auto complete using places API

--- app/src/main/java/com/example/cinemaapp2/ui/Maps/MapsActivity2.java
+++ app/src/main/java/com/example/cinemaapp2/ui/Maps/MapsActivity2.java
@@ -1,16 +1,31 @@
 package com.example.cinemaapp2.ui.Maps;
 
+import static com.example.cinemaapp2.BuildConfig.MAPS_API_KEY;
+import static com.google.android.gms.location.Priority.PRIORITY_BALANCED_POWER_ACCURACY;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.example.cinemaapp2.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,13 +36,24 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.cinemaapp2.databinding.ActivityMaps2Binding;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMaps2Binding binding;
     private FusedLocationProviderClient fusedLocationClient;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    AutocompleteSupportFragment autocompleteFragment;
 
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +76,12 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
             // Log.i("MAP, "Location permissions have not been granted");
             ActivityResultLauncher<String[]> locationPermissionRequest =
                     registerForActivityResult(new ActivityResultContracts
-                                    .RequestMultiplePermissions(), result -> {
-                                Boolean fineLocationGranted = result.getOrDefault(
+                            .RequestMultiplePermissions(), result -> {
+                        Boolean fineLocationGranted = result.getOrDefault(
                                         android.Manifest.permission.ACCESS_FINE_LOCATION, false);
-                                Boolean coarseLocationGranted = result.getOrDefault(
-                                        android.Manifest.permission.ACCESS_COARSE_LOCATION,false);
-                                if (fineLocationGranted != null && fineLocationGranted) {
+                        Boolean coarseLocationGranted = result.getOrDefault(
+                                        android.Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                        if (fineLocationGranted != null && fineLocationGranted) {
                                     Log.i("MAP", "Precise location access granted.");
                                     getLastLocation();
                                 } else if (coarseLocationGranted != null && coarseLocationGranted) {
@@ -70,7 +96,7 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
             // Before you perform the actual permission request, check whether your app
             // already has the permissions, and whether your app needs to show a permission
             // rationale dialog. For more details, see Request permissions.
-            locationPermissionRequest.launch(new String[] {
+            locationPermissionRequest.launch(new String[]{
                     android.Manifest.permission.ACCESS_FINE_LOCATION,
                     android.Manifest.permission.ACCESS_COARSE_LOCATION
             });
@@ -78,6 +104,34 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
             Log.i("MAP", "Location permissions already granted.");
             getLastLocation();
         }
+
+        Places.initialize(this, MAPS_API_KEY);
+
+        init();
+    }
+
+    public void init(){
+        // Initialize the AutocompleteSupportFragment.
+        autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                // TODO: Get info about the selected place.
+                Log.i("Places", "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                // TODO: Handle the error.
+                Log.i("Places", "An error occurred: " + status);
+            }
+        });
     }
 
     /**
@@ -97,21 +151,12 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.clear();
     }
 
     public void getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        // fusedLocationClient.getCurrentLocation(PRIORITY_BALANCED_POWER_ACCURACY, null)
+//         fusedLocationClient.getCurrentLocation(PRIORITY_BALANCED_POWER_ACCURACY, null);
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
 
@@ -122,12 +167,13 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
                             // Logic to handle location object
                             Log.i("MAP", "We got a location: (" + location.getLatitude() +
                                     ", " + location.getLongitude() + ")");
+                            LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(myLocation).title("You are here"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
                         } else {
                             Log.i("MAP", "We failed to get a last location");
                         }
                     }
                 });
     }
-
-
 }
